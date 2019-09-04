fd612965676b417fe5b7fff97ad07157939e642d
lionCL <18688788437@163.com>
2019-09-04 16:50:08 +0800

    筛选功能完成--封装列表完整函数,当参数为undefined时不会传递此参数

--- src/views/home/article/index.vue
+++ src/views/home/article/index.vue
@@ -9,32 +9,41 @@
       <!-- form表单 -->
       <el-form ref="searchForm">
         <el-form-item label="文章状态:">
+          <!-- 接口文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部 -->
           <el-radio-group v-model="searchForm.status">
-            <el-radio label="全部"></el-radio>
-            <el-radio label="草稿"></el-radio>
-            <el-radio label="待审核"></el-radio>
-            <el-radio label="审核通过"></el-radio>
-            <el-radio label="审核失败"></el-radio>
+            <el-radio label="">全部</el-radio>
+            <el-radio label="0">草稿</el-radio>
+            <el-radio label="1">待审核</el-radio>
+            <el-radio label="2">审核通过</el-radio>
+            <el-radio label="3">审核失败</el-radio>
           </el-radio-group>
         </el-form-item>
-
         <el-form-item label="频道列表:">
           <el-select placeholder="请选择"
-                     v-model="searchForm.channel">
-            <el-option label="web前端"
+                     v-model="searchForm.channel_id">
+            <el-option label="所有频道"
                        value=""></el-option>
-            <el-option label="java"
-                       value="java"></el-option>
+            <el-option :label="item.name"
+                       :value="item.id"
+                       v-for="item in channels"
+                       :key="item.id"></el-option>
           </el-select>
         </el-form-item>
-
         <el-form-item label="时间选择:">
           <el-date-picker v-model="searchForm.date"
                           type="datetimerange"
                           range-separator="至"
                           start-placeholder="开始日期"
-                          end-placeholder="结束日期">
+                          end-placeholder="结束日期"
+                          value-format="yyyy-MM-dd">
+            <!-- 设置时间格式 -->
           </el-date-picker>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary"
+                     round
+                     @click="doSearch">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -42,9 +51,8 @@
     <el-card class="bottom-card">
       <div slot="header"
            class="clearfix">
-        <span>共找到{{total}}条符合条件的内容</span>
+        <span>共找到<b>{{total}}</b>条符合条件的内容</span>
       </div>
-
       <el-table :data="tableData"
                 v-loading="isLoading"
                 style="width: 100%">
@@ -58,11 +66,9 @@
                  width="50px">
           </template>
         </el-table-column>
-
         <el-table-column prop="title"
                          label="标题">
         </el-table-column>
-
         <el-table-column prop="status"
                          label="状态"
                          width="120">
@@ -70,11 +76,9 @@
             <el-tag>{{scope.row.status | filterStatus}}</el-tag>
           </template>
         </el-table-column>
-
         <el-table-column prop="pubdate"
                          label="发布时间">
         </el-table-column>
-
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button type="primary"
@@ -93,7 +97,8 @@
                      :page-size="10"
                      layout="total, prev, pager, next"
                      :total="total"
-                     background>
+                     background
+                     :disabled="isLoading">
       </el-pagination>
 
     </el-card>
@@ -101,24 +106,25 @@
 
 </template>
 
-
 <script>
 export default {
-  name: 'my-article',
+  name: 'articleList',
   data() {
     return {
       //加载动画
       isLoading: false,
       //form表单数据
       searchForm: {
-        status: '全部',
-        channel: '',
+        status: '',
+        channel_id: '',
         date: ''
       },
       //存储table表单数据
       tableData: [],
       //信息总条数
-      total: 0
+      total: 0,
+      //频道列表
+      channels: []
     }
   },
 
@@ -137,29 +143,52 @@ export default {
     //     this.isLoading = false
     //   })
     this.getPageData(1)
+
+    //频道列表请求
+    this.$axios.get('/mp/v1_0/channels').then(res => {
+      this.channels = res.data.data.channels
+    })
   },
 
   methods: {
-    //封装分请求内容列表函数
+    //封装请求内容列表完整函数函数
     getPageData(page) {
       //发送请求时候加载动画
       this.isLoading = true
+      //接口文档中status和channel_id 不传值的话 为全部  当status为undefined时 请求文件就不会添加status 设置为空不行
+      // 数组元素不存在的时候 取下标 返回值为undefined
+      let status =
+        this.searchForm.status === '' ? undefined : this.searchForm.status
+      let channel_id =
+        this.searchForm.channel_id === ''
+          ? undefined
+          : this.searchForm.channel_id
 
       this.$axios
         .get('/mp/v1_0/articles', {
           params: {
+            status,
+            channel_id,
+            begin_pubdate: this.searchForm.date[0],
+            end_pubdate: this.searchForm.date[1],
             page
           }
         })
         .then(res => {
           this.tableData = res.data.data.results
           this.total = res.data.data.total_count
+          //关闭loading动画
           this.isLoading = false
         })
     },
-
+    //分页点击切换
     handleCurrentChange(page) {
       this.getPageData(page)
+    },
+    //筛选事件
+    doSearch() {
+      // 表单条件
+      this.getPageData(1)
     }
   },
   filters: {
