c41f84586c2a68bc2226e5a64ee0a649cc83716d
lionCL <18688788437@163.com>
2019-09-05 16:17:53 +0800

    发布页面整体布局完成, 富文本添加完成

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -4,32 +4,86 @@
          class="clearfix">
       <span>发布文章</span>
     </div>
-
     <el-form ref="formData"
              :model="formData"
-             label-width="100px">
+             label-width="80px">
       <el-form-item label="标题">
         <el-input v-model="formData.title"
                   style="width:400px"></el-input>
       </el-form-item>
 
       <el-form-item label="内容">
-
+        <quillEditor v-model="formData.content"
+                     :options="editorOption"></quillEditor>
       </el-form-item>
 
+      <el-form-item label="封面">
+      </el-form-item>
+
+      <el-form-item label="频道">
+
+      </el-form-item>
+      <!-- 分割线 -->
+      <el-divider></el-divider>
+
+      <el-form-item>
+        <el-button type="primary"
+                   size="small">草稿</el-button>
+        <el-button type="primary"
+                   size="small">发布文章...</el-button>
+      </el-form-item>
     </el-form>
   </el-card>
 </template>
 
 <script>
+// 富文本导包
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
 export default {
   name: 'publish',
   data() {
     return {
+      //表单数据
       formData: {
-        title: ''
+        title: '',
+        content: ''
+      },
+      //富文本参数配置
+      editorOption: {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+            ['blockquote', 'code-block'],
+
+            [{ header: 1 }, { header: 2 }], // custom button values
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+            [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+            [{ direction: 'rtl' }], // text direction
+
+            [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+            [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+            [{ font: [] }],
+            [{ align: [] }],
+            ['clean'], // remove formatting button
+            ['image'],
+            ['video'],
+            ['link']
+          ]
+        }
       }
     }
+  },
+  components: {
+    quillEditor
   }
 }
 </script>
@@ -41,6 +95,10 @@ export default {
       font-size: 14px;
       color: #323745;
     }
+  }
+
+  .ql-editor {
+    height: 400px;
   }
 }
 </style>
